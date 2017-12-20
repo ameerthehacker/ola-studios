f9fc5dcc7c4cbd08cc1cc6a4e9100c4ca09ba24b
ameerthehacker <ameerjhanprof@gmail.com>
2017-12-20 09:46:17 +0530

    Journey of ola studios begins

---
+++ app/src/main/java/com/example/ameerthehacker/olastudios/Song.java
@@ -0,0 +1,54 @@
+package com.example.ameerthehacker.olastudios;
+
+/**
+ * Created by ameerthehacker on 20/12/17.
+ */
+
+import java.util.*;
+
+public class Song {
+
+    private String name;
+    private String url;
+    private ArrayList<String> artists;
+    private String coverImage;
+
+    Song(String name, String url, ArrayList<String> artists, String coverImage) {
+        this.name = name;
+        this.url = url;
+        this.artists = artists;
+        this.coverImage = coverImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public ArrayList<String> getArtists() {
+        return artists;
+    }
+
+    public void setArtists(ArrayList<String> artists) {
+        this.artists = artists;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+}
